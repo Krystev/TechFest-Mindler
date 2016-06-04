00312ab94b319ec43489377e5212421b4cf89816
Nikolay Nikolov <nikolay.nikolov92@gmail.com>
2016-06-04 17:14:14 +0300

    QuestionActivitiesScreen

---
+++ Mindler/app/src/main/java/com/inveitix/mindler/ui/Questions.java
@@ -0,0 +1,54 @@
+package com.inveitix.mindler.ui;
+
+/**
+ * Created by toshiba on 4.6.2016 г..
+ */
+public class Questions {
+
+    String question;
+    String answerA;
+    String answerB;
+    String answerC;
+    String answerD;
+
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswerA() {
+        return answerA;
+    }
+
+    public void setAnswerA(String answerA) {
+        this.answerA = answerA;
+    }
+
+    public String getAnswerB() {
+        return answerB;
+    }
+
+    public void setAnswerB(String answerB) {
+        this.answerB = answerB;
+    }
+
+    public String getAnswerC() {
+        return answerC;
+    }
+
+    public void setAnswerC(String answerC) {
+        this.answerC = answerC;
+    }
+
+    public String getAnswerD() {
+        return answerD;
+    }
+
+    public void setAnswerD(String answerD) {
+        this.answerD = answerD;
+    }
+}
